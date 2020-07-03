497eeb618e6ff923592c0060c2ceb3f6ca83bcdb
7170 <aaguirre@atixlabs.com>
2020-07-03 18:24:05 -0300

    fix: Refactor txOut ADA setter

--- src/generators/tx.js
+++ src/generators/tx.js
@@ -3,13 +3,39 @@ const {
   helpers: { randomNumber }
 } = require('../utils/data-types');
 
+const setAdaInTxOutsWithoutTxIns = ({ txOuts, txIns, totalAda }) => {
+  const txOutsWithoutTxsIn = [];
+  const epochsAmount = txOuts.length;
+  const txInsFlatted = txIns.flat();
+  for (let currentEpoch = 0; currentEpoch < epochsAmount; currentEpoch++) {
+    const txsAmount = txOuts[currentEpoch].length;
+    txOutsWithoutTxsIn[currentEpoch] = [];
+    for (let currentTx = 0; currentTx < txsAmount; currentTx++) {
+      const txOut = txOuts[currentEpoch][currentTx];
+      // If that txOut doesn't have a txIn add to the txOuts that will be setted
+      if (txInsFlatted.findIndex(txIn => txIn.tx_out_id === txOut.id) === -1)
+        txOutsWithoutTxsIn[currentEpoch].push(txOut);
+    }
+  }
+  for (let currentEpoch = 0; currentEpoch < epochsAmount; currentEpoch++) {
+    let remainingAda = totalAda[currentEpoch];
+    const txOutsAmount = txOutsWithoutTxsIn[currentEpoch].length;
+    for (let currentTxOut = 0; currentTxOut < txOutsAmount; currentTxOut++) {
+      const adaInTxOut =
+        currentTxOut === txOutsAmount ? remainingAda : randomNumber(0, remainingAda);
+      txOutsWithoutTxsIn[currentEpoch][currentTxOut].value = adaInTxOut;
+      remainingAda -= adaInTxOut;
+    }
+  }
+  return txOutsWithoutTxsIn;
+};
+
 exports.generateTx = (blockId, fee, outSum) => txGen.generate(blockId, { fee, outSum });
 
 exports.generateTxInsOuts = ({ txs, txOuts, utxoCount, totalAda }) => {
   const epochsAmount = txs.length;
   const txIns = [];
   for (let currentEpoch = 0; currentEpoch < epochsAmount; currentEpoch++) {
-    let remainingAda = totalAda[currentEpoch];
     const txOutsWithTxIns = [...txOuts[currentEpoch]];
     const txsWithTxIns = [...txs];
     let i = 0;
@@ -27,20 +53,12 @@ exports.generateTxInsOuts = ({ txs, txOuts, utxoCount, totalAda }) => {
       const [txOut] = txOutsWithTxIns.splice(txIndex, 1);
       if (!txIns[currentEpoch]) txIns[currentEpoch] = [];
       txIns[currentEpoch].push(txInGen.generate(tx.id, txOut.id, txOut.index));
-      // Set ADA in that txOut
-      const adaInTxOut =
-        i === txOuts[currentEpoch].length - utxoCount[currentEpoch] - 1
-          ? remainingAda
-          : randomNumber(0, remainingAda);
-      txOuts[currentEpoch][
-        txOuts[currentEpoch].findIndex(({ id }) => txOut.id === id)
-      ].value = adaInTxOut;
-      remainingAda -= adaInTxOut;
       i++;
     }
   }
+  const txOutsWithSettedAdaValue = setAdaInTxOutsWithoutTxIns({ txOuts, txIns, totalAda });
 
-  return { txIns: txIns.flat(), txOuts: txOuts.flat() };
+  return { txIns: txIns.flat(), txOuts: txOutsWithSettedAdaValue.flat() };
 };
 
 exports.generateTxOut = txid => txOutGen.generate(txid);
